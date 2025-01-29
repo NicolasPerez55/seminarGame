9babfde1c75b416f4a9f3eada635e17fdb62f8a0
1NTROO <95148848+1NTROO@users.noreply.github.com>
2025-01-29 14:48:29 +0100

    camera zooms out now

--- SeminarGame/Assets/Scripts/Tetris/CameraHandler.cs
+++ SeminarGame/Assets/Scripts/Tetris/CameraHandler.cs
@@ -6,6 +6,8 @@ public class CameraHandler : MonoBehaviour
 {
     public Vector2 velocity;
 
+    public int zoomIndex = 0;
+
     public float damp = .96f;
     public float spring = .3f;
 
@@ -16,10 +18,22 @@ public class CameraHandler : MonoBehaviour
         velocity -= (Vector2)transform.position * spring * Time.deltaTime;
 
         transform.position += (Vector3)velocity * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            IncrementZoom();
+        }
     }
 
     public void Shake(Vector2 direction)
     {
         velocity += direction;
+    }
+
+    public void IncrementZoom()
+    {
+        zoomIndex++;
+        if (zoomIndex <= 5)
+            Camera.main.orthographicSize += 0.66f;
     }
 }
