e3a05bfd152bca9ae7305c32a13d47a56b3ee466
1NTROO <95148848+1NTROO@users.noreply.github.com>
2025-01-30 17:15:46 +0100

    good ending

---
+++ SeminarGame/Assets/Scripts/Escape.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using KinematicCharacterController.Examples;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Escape : MonoBehaviour
+{
+    [Header("CanvasItems")]
+    [SerializeField] private GameObject escapeCanvas;
+    [SerializeField] private GameObject escapeButton;
+    [SerializeField] private Image escapeTV;
+
+
+    [Header("Player")]
+    [SerializeField] private Transform player;
+    [SerializeField] private GameObject cameraController;
+
+
+    private bool _isEscaping = false;
+    private float _timer = 0f;
+    private float _maxTimer = 3f;
+    void Start()
+    {
+        escapeCanvas.SetActive(false);
+        escapeButton.SetActive(false);
+        escapeTV.enabled = false;    
+    }
+
+    void Update()
+    {
+        if (_isEscaping)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            escapeTV.transform.localScale -= new Vector3(0.002f, 0.002f, 0.002f);
+            if (escapeTV.transform.localScale.x <= 1.1f)
+            {
+                escapeTV.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+            }
+            _timer += Time.deltaTime;
+            if (_timer >= _maxTimer)
+            {
+                EscapeSuccess();
+            }
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0) && !_isEscaping)
+        {
+            if (Vector3.Distance(player.position, transform.position) < 15)
+            {
+                TryEscape();
+            }
+        }
+    }
+
+    void TryEscape()
+    {
+        escapeCanvas.SetActive(true);
+        escapeTV.enabled = true;
+        _isEscaping = true;
+    }
+
+    void EscapeSuccess()
+    {
+        escapeButton.SetActive(true);
+        cameraController.GetComponent<ExamplePlayer>().enabled = false;
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
